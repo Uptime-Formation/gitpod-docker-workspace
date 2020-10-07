b53b1ff19a8be11f5c4000e2335806b666c6a834
Hadrien <26697460+ketsapiwiq@users.noreply.github.com>
2020-10-07 16:30:34 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,2 +1,7 @@
+brew install docker
 brew update # if it gets stuck kill it
-brew install docker docker-compose
+brew install docker-compose
+# if it doesn't work just do:
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o bin/docker-compose
+chmod +x bin/docker-compose
+export PATH="./bin:$PATH"
